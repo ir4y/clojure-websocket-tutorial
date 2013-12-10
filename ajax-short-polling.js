d6a86a6b9b5e1d0717df4342e8b184edb450adbb
Ilya Beda <ir4y.ix@gmail.com>
2013-12-10 15:03:35 +0700

    Update ajax-short-polling.js

--- ajax-short-polling.js
+++ ajax-short-polling.js
@@ -1,6 +1,6 @@
 function doPoll(){
  $.post('ajax/test.html', function(data) {
-  alert(data);  // process results here
+  alert(data);  // process results here
   setTimeout(doPoll,5000);
   });
 }
