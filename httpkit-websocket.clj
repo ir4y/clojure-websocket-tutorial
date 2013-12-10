7221dea68f426fc27581e5d6e0374a842c3d5a89
Ilya Beda <ir4y.ix@gmail.com>
2013-12-10 15:04:32 +0700

    Update httpkit-websocket.clj

--- httpkit-websocket.clj
+++ httpkit-websocket.clj
@@ -1,8 +1,8 @@
 (defn handler [request]
-  (with-channel request channel
-    (on-close channel (fn [status] (println "channel closed: " status)))
-    (on-receive channel (fn [data] ;; echo it back
-                          (send! channel data)))))
+ (with-channel request channel
+  (on-close channel (fn [status] (println "channel closed: " status)))
+  (on-receive channel (fn [data]
+                       (send! channel data)))))
 
 (run-server handler {:port 8000})
 
